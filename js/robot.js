b5bdb888f1581a35e4fab5d16a1a53a13babbf6a
MLee21 <minnie.lee89@gmail.com>
2015-07-26 20:37:34 -0600

    created robot with methods to turn right and left.

---
+++ js/robot.js
@@ -0,0 +1,45 @@
+"use strict";
+
+function Robot() {
+    this.x = null;
+    this.y = null;
+    this.orientation = null;
+    this.maze = null;
+}
+
+Robot.prototype.setMaze = function(maze) {
+    this.maze = maze;
+    this.x = maze.startX;
+    this.y = maze.startY;
+    this.orientation = maze.startOrientation;
+};
+
+Robot.prototype.turnRight = function() {
+    if (!this.maze || !this.maze.isValidDirection(this.orientation)) {
+        return false;
+    }
+
+    var rights = {
+        north: "east",
+        east: "south",
+        south: "west",
+        west: "north"
+    }
+    this.orientation = rights[this.orientation];
+    return true;
+}
+
+Robot.prototype.turnLeft = function() {
+    if (!this.maze || !this.maze.isValidDirection(this.orientation)) {
+        return false;
+    }
+
+    var lefts = {
+        north: "west",
+        east: "north",
+        south: "east",
+        west: "south"
+    }
+    this.orientation = lefts[this.orientation];
+    return true;
+}

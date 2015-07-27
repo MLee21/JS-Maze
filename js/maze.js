d4d06dc889b34977d8349cb53d3d893db47c6040
MLee21 <minnie.lee89@gmail.com>
2015-07-26 19:25:08 -0600

    add more attributes to object.

--- js/maze.js
+++ js/maze.js
@@ -1,6 +1,12 @@
 "use strict";
 
-function Maze() {
-  this.width = 7;
-  this.height = 5;
+function Maze(width, height) {
+  this.width            = width;
+  this.height           = height;
+
+  this.startX           = null;
+  this.startY           = null;
+  this.startOrientation = null;
+  this.endX             = null;
+  this.endY             = null;
 }
